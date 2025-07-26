350f79825a9922076964b3db85be448e3392f5fa
bgsourav <bgsouravbg@gmail.com>
2025-07-26 20:00:20 +0530

    Rebuilder4.0

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -171,7 +171,7 @@ const Dashboard = () => {
             size="sm"
           >
             <Settings className="w-4 h-4 mr-2" />
-            Topic
+            Topics
           </Button>
         </div>
         {/* Alerts Panel */}
